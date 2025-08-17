3b2cbc065445d55352459656237ed66ddbb46c01
Srinath Akkem <reddyakkem@blackduck.com>
2025-08-17 11:25:40 +0530

    Update MD5Hashing.java
    Co-authored-by: SrinathAkkem-bd <infipara.edu@gmail.com>

--- src/main/java/com/hospitalmanagement/hashing/MD5Hashing.java
+++ src/main/java/com/hospitalmanagement/hashing/MD5Hashing.java
@@ -4,31 +4,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MD5Hashing {
-	
-	//MD5 Hashing Algorithm
-	public static String doHashing (String password)
-	{
-		  StringBuilder sb = null;
-		  try {
-		   MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-		   
-		   messageDigest.update(password.getBytes());
-		   
-		   byte[] resultByteArray = messageDigest.digest();
-		   
-		   sb = new StringBuilder();
-		   
-		   for (byte b : resultByteArray) {
-		    sb.append(String.format("%02x", b));
-		   }
-		   
-		   
-		   
-		  } catch (NoSuchAlgorithmException e) {
-		   e.printStackTrace();
-		  }
-		 
-		  return sb.toString();
-    }
 
+    public static String doHashing(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
